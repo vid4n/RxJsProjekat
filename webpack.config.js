3e09932d196dec80c2bfeebee37d16a52ca03ee2
Danilo <danilovidanovic@elfak.rs>
2020-05-02 02:50:58 +0200

    Babel added

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,46 @@
-const path = require('path');
+// const path = require('path');
+
+// module.exports = {
+//   entry: './src/index.js',
+//   devtool: 'inline-source-map',
+//   // devServer: {
+//   //   contentBase: './dist',
+//   // },
+//   output: {
+//     path: path.resolve(__dirname, 'dist'),
+//     filename: 'bundle.js',
+//   },
+//   devServer: {
+//     contentBase: './',
+//   },
+// };
+const path = require ('path');
 
 module.exports = {
-  entry: './src/index.js',
+  mode: 'development',
+  entry: ['@babel/polyfill', './src/index.js'],
+
   devtool: 'inline-source-map',
-  // devServer: {
-  //   contentBase: './dist',
-  // },
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve (__dirname, 'dist'),
     filename: 'bundle.js',
   },
   devServer: {
     contentBase: './',
+  },
+  module: {
+    rules: [
+      {
+        //kako ce se obraditi fajlovi
+        test: /\.js$/, //regularni izraz $-ako se string zavrsava sa tom reci
+        exclude: /(node_modules|bower_components)/, //da ne trazi node_modules jer ima mng fajlova
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
   },
 };

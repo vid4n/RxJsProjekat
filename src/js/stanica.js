33f7e0081f51a3872a340c03d69b681dd729dec7
Danilo <danilovidanovic@elfak.rs>
2020-05-03 02:08:00 +0200

    Zapoceta klasa Stanica

---
+++ src/js/stanica.js
@@ -0,0 +1,9 @@
+export class Stanica{
+
+    constructor(id, kapacitetEnergije, stanjeEnergije, cenaPoJedinici){
+        this.id = id;
+        this.kapacitetEnergije = kapacitetEnergije;
+        this.stanjeEnergije = stanjeEnergije;
+        this.cenaPoJedinici = cenaPoJedinici;
+    }
+}
